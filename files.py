47ccb18f15e50c4b1553153455446a788e4c906c
toxa <linn84@mail.ru>
2022-11-03 17:44:33 +0300

    first commit

---
+++ files.py
@@ -0,0 +1,33 @@
+# работа с файлами
+
+#Примеры
+
+# эта программа пишет три строки данных в файл
+
+# def main():
+#     #Открываем файл с именем phylosophers.txt
+#     outfile = open('./txtfiles/phylosophers.txt', 'w')
+#
+#     #Записываем данные в файл
+#     outfile.write('Фредрих Нитсше\n')
+#     outfile.write('Василий Пупкин\n')
+#     outfile.write('Киса Воробьянинов\n')
+#
+#     #Закрываем файл
+#     outfile.close()
+# #
+#
+# #  открываем файл и считываем его содержимое
+#
+#
+#     infile = open('./txtfiles/phylosophers.txt', 'r')
+#     file_contetnts = infile.read()
+#     infile.close()
+#
+#
+#     print(file_contetnts)
+#
+# if __name__ == "__main__":
+#     main()
+
+
